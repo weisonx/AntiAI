c9e1180a20958879949ee36c09bd96944b8ffb68
weisonx <578822454@qq.com>
2023-06-12 19:34:03 +0800

    Update manageCoupon.js

--- Server/manageCoupon.js
+++ Server/manageCoupon.js
@@ -20,40 +20,40 @@ const {
         console.log(`生成兑换券: ${code}`);
       }
   
-      // 测试返回一些兑换券并打印到控制台
-      const numCouponsToReturn = 5;
-      for (let i = 0; i < numCouponsToReturn; i++) {
-        const couponCode = await findUnusedCoupon();
-        console.log(`返回兑换券: ${couponCode}`);
-      }
+      // // 测试返回一些兑换券并打印到控制台
+      // const numCouponsToReturn = 5;
+      // for (let i = 0; i < numCouponsToReturn; i++) {
+      //   const couponCode = await findUnusedCoupon();
+      //   console.log(`返回兑换券: ${couponCode}`);
+      // }
   
-      // 模拟卖出一些兑换券
-      const numCouponsToSell = 3;
-      for (let i = 0; i < numCouponsToSell; i++) {
-        const couponCode = await findUnusedCoupon();
-        await markCouponAsSold(couponCode);
-        console.log(`已卖出兑换券: ${couponCode}`);
-      }
+      // // 模拟卖出一些兑换券
+      // const numCouponsToSell = 3;
+      // for (let i = 0; i < numCouponsToSell; i++) {
+      //   const couponCode = await findUnusedCoupon();
+      //   await markCouponAsSold(couponCode);
+      //   console.log(`已卖出兑换券: ${couponCode}`);
+      // }
   
-      // 模拟使用一些兑换券
-      const numCouponsToUse = 2;
-      for (let i = 0; i < numCouponsToUse; i++) {
-        const couponCode = await findUnusedCoupon();
-        await markCouponAsUsed(couponCode);
-        console.log(`已使用兑换券: ${couponCode}`);
-      }
+      // // 模拟使用一些兑换券
+      // const numCouponsToUse = 2;
+      // for (let i = 0; i < numCouponsToUse; i++) {
+      //   const couponCode = await findUnusedCoupon();
+      //   await markCouponAsUsed(couponCode);
+      //   console.log(`已使用兑换券: ${couponCode}`);
+      // }
   
-      // 模拟使用一个已被使用的兑换券而返回失败
-      const usedCouponCode = await findUnusedCoupon();
-      await markCouponAsUsed(usedCouponCode);
-      console.log(`尝试使用已被使用的兑换券: ${usedCouponCode}`);
+      // // 模拟使用一个已被使用的兑换券而返回失败
+      // const usedCouponCode = await findUnusedCoupon();
+      // await markCouponAsUsed(usedCouponCode);
+      // console.log(`尝试使用已被使用的兑换券: ${usedCouponCode}`);
   
-      const isUsed = await isCouponUsed(usedCouponCode);
-      if (isUsed) {
-        console.log(`兑换券使用失败`);
-      } else {
-        console.log(`兑换券使用成功`);
-      }
+      // const isUsed = await isCouponUsed(usedCouponCode);
+      // if (isUsed) {
+      //   console.log(`兑换券使用失败`);
+      // } else {
+      //   console.log(`兑换券使用成功`);
+      // }
   
     } catch (error) {
       console.error('测试出错:', error);
